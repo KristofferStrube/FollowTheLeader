83c605a16a21388148fbba8ec6266a84993ba6c1
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-11-05 14:02:49 +0100

    Added GitHub Pages

--- FollowTheLeader.Shared/Round.cs
+++ FollowTheLeader.Shared/Round.cs
@@ -2,7 +2,7 @@
 
 public class Round
 {
-    public decimal Time { get; set; } = 5;
+    public decimal Time { get; set; } = 20;
     public Dictionary<string, int> Points { get; set; } = new();
     public string Leader { get; set; } = "";
 }
